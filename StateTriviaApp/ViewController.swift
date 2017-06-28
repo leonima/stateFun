9c1190b072dbbce6302774292de7d8e78d3821fe
Sakina Ali <sali2@sakinas-macbook-air.local>
2017-06-28 12:57:49 -0500

    Initial Commit

---
+++ StateTriviaApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StateTriviaApp
+//
+//  Created by Sakina Ali on 6/28/17.
+//  Copyright © 2017 Sakina Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ StateTriviaAppTests/StateTriviaAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  StateTriviaAppTests.swift
+//  StateTriviaAppTests
+//
+//  Created by Sakina Ali on 6/28/17.
+//  Copyright © 2017 Sakina Ali. All rights reserved.
+//
+
+import XCTest
+@testable import StateTriviaApp
+
+class StateTriviaAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
